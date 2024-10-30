9e911de76d4524fad9b73dc55e1ae97e9f07ba73
bnqy <obbonobo@gmail.com>
2024-10-30 23:36:11 +0600

    log config

---
+++ CompanyStaff/Extensions/ServiceExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Contracts;
+using LoggerService;
+
+namespace CompanyStaff.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigCORS(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicyAnyOrigin", builder =>
+                {
+                    builder.AllowAnyOrigin();
+                    builder.AllowAnyMethod();
+                    builder.AllowAnyHeader();
+                });
+            });
+
+        public static void ConfigIISIntegration(this IServiceCollection services) => 
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+
+        public static void ConfigLoggerService(this IServiceCollection services) => 
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+    }
+}
--- CompanyStaff/Program.cs
+++ CompanyStaff/Program.cs
@@ -1,6 +1,17 @@
+using CompanyStaff.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), @"\nlog.config"));
+//Console.WriteLine(string.Concat(Directory.GetCurrentDirectory(), @"\nlog.config"));
+//LogManager.Setup().LoadConfigurationFromFile(string.Concat(Directory.GetCurrentDirectory(), @"\nlog.config"));
+
+builder.Services.ConfigCORS();
+builder.Services.ConfigIISIntegration();
+builder.Services.ConfigLoggerService();
 
 builder.Services.AddControllers();
 
@@ -8,7 +19,20 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+
+
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicyAnyOrigin");
 
 app.UseAuthorization();
 
